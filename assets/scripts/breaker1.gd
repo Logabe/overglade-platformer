6e127c7345d764df2bb3873b3ebea6106b53dbde
Ashkesh-D <ashlesh.deshmukh.sg@gmail.com>
2026-01-30 23:04:04 +0800

    proto level building 1

---
+++ assets/scripts/breaker1.gd
@@ -0,0 +1,36 @@
+extends CSGBox3D
+
+@onready var label_3d: Label3D = $Label3D
+@onready var area_3d: Area3D = $Area3D
+@onready var collision_shape_3d: CollisionShape3D = $Area3D/CollisionShape3D
+var player_inside := false
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	area_3d.connect("body_entered", Callable(self, "_on_body_entered"))
+	area_3d.connect("body_exited", Callable(self, "_on_body_exited"))
+	label_3d.hide()
+
+
+signal collect1()
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player_inside and Input.is_action_just_pressed("pickup"):
+		self.hide()
+		self.set_physics_process(false)
+		emit_signal("collect1")
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		label_3d.show()
+		player_inside = true
+		
+		
+func _on_body_exited(body):
+	if body.is_in_group("player"):
+		label_3d.hide()
+		player_inside = false
+
+
+func _on_button_pressed() -> void:
+	self.show()
+	self.set_physics_process(true)
