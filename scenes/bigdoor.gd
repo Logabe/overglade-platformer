60e92d61c890318e7f5e72da8a16b8e62098b148
Logan Bentley <logangbentley@gmail.com>
2026-02-02 04:54:36 +0800

    Finished texturing level

--- scenes/bigdoor.gd
+++ scenes/bigdoor.gd
@@ -2,10 +2,12 @@ extends Area3D
 
 
 @onready var collider = $StaticBody3D
+@onready var collider2 = $StaticBody3D2
 var open := false
 
 func _physics_process(delta: float) -> void:
-	collider.position.x = move_toward(collider.position.x, 1 if open else 0, delta * 3)
+	collider.position.x = move_toward(collider.position.x, 1.75 if open else 0.75, delta * 3)
+	collider2.position.x = move_toward(collider2.position.x, -1.75 if open else -.75, delta * 3)
 
 func _on_body_exited(body: Node3D) -> void:
 	open = false
